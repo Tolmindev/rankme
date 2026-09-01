837ec3f3fd7c2f3d7bfaf783a5483824d247aa12
Tolmindev <Tolmindev@outlook.com>
2026-09-02 00:07:54 +0300

    oo

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -562,7 +562,7 @@ function expertBadgeHtml(opts) {
   if (!on && !clickable) return '';
   var cls = 'expert-badge' + (on ? ' is-on' : ' is-off');
   var label = on ? 'RankMe Expert' : 'Become a RankMe Expert';
-  var src = on ? 'assets/icons/expert-badge.svg' : 'assets/icons/expert-badge-off.svg';
+  var src = on ? 'assets/icons/expert-badge.svg?v=20260901am' : 'assets/icons/expert-badge-off.svg';
   var inner = '<img src="' + src + '" alt="">';
   if (on) {
     return '<span class="' + cls + '" title="' + label + '" aria-label="' + label + '">' + inner + '</span>';
